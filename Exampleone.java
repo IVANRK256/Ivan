122fa0c661bbdb83355488c8969661e63d9331bd
IVANRK256 <ivantwongyeirwe5@gmail.com>
2024-09-12 10:14:11 +0300

    Create Exampleone.java

---
+++ Exampleone.java
@@ -0,0 +1,21 @@
+public class exampleone {
+  public static void main(String args[]){ 
+  //arithemetic operators
+  int a=10;
+  int b=5;
+  //addition
+  int sum =a+b;
+  //subtraction
+  int difference =a-b;
+  //multiplication
+  int product=a*b;
+  //Division
+  int quotient=a/b;
+  System.out.println("sum is:"+sum);
+  System.out.println("difference is:"+difference);
+  System.out.println("product is:"+product);
+  System.out.println("quotient is:"+quotient);
+
+  
+  }
+}
